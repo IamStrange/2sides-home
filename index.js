d2059a95626b283018470993e431d59db33e9fe7
TheReturn <replacedstrange@gmail.com>
2017-04-02 13:57:34 -0700

    done

--- index.js
+++ index.js
@@ -1,12 +1,66 @@
-var express = require('express')
-var app = express()
-var path    = require("path");
+'use strict';
 
+const express    = require('express')
+const app        = express()
+const path       = require("path");
+const bodyParser = require('body-parser')
+const nodemailer = require('nodemailer');
+const validator = require('validator');
+
+// create reusable transporter object using the default SMTP transport
+let transporter  = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.gmailUser,
+        pass: process.env.gmailPass
+    }
+});
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.set('view engine', 'pug')
+app.set('views', './static')
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname+'/index.html'));
+  var success = req.query.success
+  res.render('index', {success: success})
 })
+
+app.post('/contact', function (req, res){
+  let name = req.body.name; 
+  let email = req.body.email; 
+  let message = req.body.message;
+
+  var valueString = "&name=" + name + "&email=" + email + "&message=" + message
+
+  if (!validator.isAscii(name)) {
+    res.redirect("/contact?error=name" + valueString); 
+    return
+  } else if (!validator.isEmail(email)) {
+    res.redirect("/contact?error=email" + valueString); 
+    return
+  } else if (message == "") {
+    res.redirect("/contact?error=message" + valueString)
+    return 
+  }
+
+  let mailOptions = {
+    from: '"' + name + '" <'+email+'>', // sender address
+    to: process.env.ToEmail, // list of receivers
+    subject: '2-Sides Inquiry ✔', // Subject line
+    text: message, // plain text body
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    res.redirect("/?success=true")
+  });
+});
+
+app.get('/contact', function (req, res){
+  res.render('contact', {error: req.query.error, name: req.query.name, email: req.query.email, message: req.query.message})
+}); 
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
